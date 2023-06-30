1acbc58f68b00a8fb04c2bcb91ebf423810cb3e2
lucassabatini <lucas.frsabatini@gmail.com>
2023-06-29 23:18:45 -0300

    Criação de duas exceções para verificar incongruências durante o uso do software

---
+++ CalculadoraPrecificacaoPC/src/main/java/exceptions/CampoVazioException.java
@@ -0,0 +1,15 @@
+package exceptions;
+
+public class CampoVazioException extends RuntimeException {
+
+    private final String campoVazio;
+
+    public CampoVazioException(String campoVazio) {
+        this.campoVazio = campoVazio;
+    }
+
+    public String getMessage() {
+        return String.format("O campo '%s' está vazio. Atualize as informações desta matéria-prima ou calcule-a novamente.",
+                campoVazio);
+    }
+}
---
+++ CalculadoraPrecificacaoPC/src/main/java/exceptions/IdNaoEncontradoException.java
@@ -0,0 +1,13 @@
+package exceptions;
+
+public class IdNaoEncontradoException extends RuntimeException {
+    private final int id;
+
+    public IdNaoEncontradoException(int id) {
+        this.id = id;
+    }
+
+    public String getMessage() {
+        return String.format("A matéria-prima com ID: '%s' não foi encontrada! Consulte os IDs no menu inicial.", id);
+    }
+}
