fc39e051b306bdaaa8a56d4908bea1609f21a0cf
lucassabatini <lucas.frsabatini@gmail.com>
2023-07-09 14:29:24 -0300

    Criação da classe ApplicationControllerAdvice para lidar com as exceções em tempo de execução

--- CalculadoraPrecificacaoPC/src/main/java/br/com/sabatini/controller/ApplicationControllerAdvice.java
+++ CalculadoraPrecificacaoPC/src/main/java/br/com/sabatini/controller/ApplicationControllerAdvice.java
@@ -1,5 +1,6 @@
 package br.com.sabatini.controller;
 
+import br.com.sabatini.exception.CampoVazioException;
 import br.com.sabatini.exception.IdNaoEncontradoException;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -12,6 +13,12 @@ public class ApplicationControllerAdvice {
     @ExceptionHandler(IdNaoEncontradoException.class)
     @ResponseStatus(HttpStatus.NOT_FOUND)
     public String handleIdNaoEncontradoException(IdNaoEncontradoException ex) {
+        return ex.getMessage();
+    }
+
+    @ExceptionHandler(CampoVazioException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public String handleCampoVazioException(CampoVazioException ex) {
         return ex.getMessage();
     }
 
