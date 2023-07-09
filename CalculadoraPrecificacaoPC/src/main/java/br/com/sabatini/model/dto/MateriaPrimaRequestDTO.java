49bdf09116771c5a39578acce51cddc9796aa026
lucassabatini <lucas.frsabatini@gmail.com>
2023-07-09 18:36:31 -0300

    Addequação da classe MateriaPrimaRequestDTO para melhor delegação de responsabilidades e maior segurança ao isolar as camadas de serviços e acesso aos dados da camada de controle

--- CalculadoraPrecificacaoPC/src/main/java/br/com/sabatini/model/dto/MateriaPrimaRequestDTO.java
+++ CalculadoraPrecificacaoPC/src/main/java/br/com/sabatini/model/dto/MateriaPrimaRequestDTO.java
@@ -2,5 +2,5 @@ package br.com.sabatini.model.dto;
 
 import java.math.BigDecimal;
 
-public record MateriaPrimaRequestDTO(String nomeMP, BigDecimal precoPagoMP, BigDecimal pesoUsadoFormulacaoMP, BigDecimal pesoCompradoMP, BigDecimal gastoFinalMP) {
+public record MateriaPrimaRequestDTO(String nomeMP, BigDecimal precoPagoMP, BigDecimal pesoUsadoFormulacaoMP, BigDecimal pesoCompradoMP) {
 }
