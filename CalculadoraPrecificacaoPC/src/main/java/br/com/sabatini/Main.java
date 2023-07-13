9ee876570fca44ea37871b95ef45e267420d7855
lucassabatini <lucas.frsabatini@gmail.com>
2023-07-12 21:39:25 -0300

    Exclusão das classes DAO e DAOMateriaPrima. A classe MateriaPrimaRepository agora é responsável por todas as operações CRUD

--- CalculadoraPrecificacaoPC/src/main/java/br/com/sabatini/Main.java
+++ CalculadoraPrecificacaoPC/src/main/java/br/com/sabatini/Main.java
@@ -1,18 +1,11 @@
 package br.com.sabatini;
 
-import br.com.sabatini.view.TelaPrincipal;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Main {
     public static void main(String[] args) {
-
         SpringApplication.run(Main.class, args);
-
-        TelaPrincipal telaPrincipal = new TelaPrincipal();
-
-        System.out.println("*** Seja bem-vinda! ***");
-        telaPrincipal.iniciarAplicativo();
     }
 }
