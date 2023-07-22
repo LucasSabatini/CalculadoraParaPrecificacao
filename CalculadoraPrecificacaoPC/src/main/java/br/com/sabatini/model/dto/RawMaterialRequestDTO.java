3b4d4c81afd08285243fabc74cce4d08ab45172e
lucassabatini <lucas.frsabatini@gmail.com>
2023-07-21 21:53:34 -0300

    Adequação dos records DTO com o novo atributo User

--- CalculadoraPrecificacaoPC/src/main/java/br/com/sabatini/model/dto/RawMaterialRequestDTO.java
+++ CalculadoraPrecificacaoPC/src/main/java/br/com/sabatini/model/dto/RawMaterialRequestDTO.java
@@ -1,6 +1,8 @@
 package br.com.sabatini.model.dto;
 
+import br.com.sabatini.model.entity.User;
+
 import java.math.BigDecimal;
 
-public record RawMaterialRequestDTO(String nameRawMaterial, BigDecimal pricePaid, BigDecimal weightUsedInRecipe, BigDecimal weightPurchased) {
+public record RawMaterialRequestDTO(String nameRawMaterial, BigDecimal pricePaid, BigDecimal weightUsedInRecipe, BigDecimal weightPurchased, User user) {
 }
--- CalculadoraPrecificacaoPC/src/main/java/br/com/sabatini/model/dto/RawMaterialResponseDTO.java
+++ CalculadoraPrecificacaoPC/src/main/java/br/com/sabatini/model/dto/RawMaterialResponseDTO.java
@@ -1,13 +1,15 @@
 package br.com.sabatini.model.dto;
 
 import br.com.sabatini.model.entity.RawMaterial;
+import br.com.sabatini.model.entity.User;
 
 import java.math.BigDecimal;
 
-public record RawMaterialResponseDTO(Long id, String nameRawMaterial, BigDecimal pricePaid, BigDecimal weightUsedInRecipe, BigDecimal weightPurchased, BigDecimal finalCost) {
+public record RawMaterialResponseDTO(Long id, String nameRawMaterial, BigDecimal pricePaid, BigDecimal weightUsedInRecipe, BigDecimal weightPurchased, BigDecimal finalCost, User user) {
 
     public RawMaterialResponseDTO(RawMaterial rawMaterial) {
         this(rawMaterial.getId(), rawMaterial.getNameRawMaterial(), rawMaterial.getPricePaid(),
-                rawMaterial.getWeightUsedInRecipe(), rawMaterial.getWeightPurchased(), rawMaterial.getFinalCost());
+                rawMaterial.getWeightUsedInRecipe(), rawMaterial.getWeightPurchased(),
+                rawMaterial.getFinalCost(), rawMaterial.getUser());
     }
 }
