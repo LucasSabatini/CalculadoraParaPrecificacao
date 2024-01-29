ed1611521353ec866331e3119518b749823a9ad8
lucassabatini <lucas.frsabatini@gmail.com>
2024-01-29 13:07:05 -0300

    Classes atualizadas para substituir métodos depreciados

--- CalculadoraPrecificacaoPC/src/main/java/br/com/sabatini/security/SecurityConfiguration.java
+++ CalculadoraPrecificacaoPC/src/main/java/br/com/sabatini/security/SecurityConfiguration.java
@@ -3,10 +3,10 @@ package br.com.sabatini.security;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
@@ -21,14 +21,18 @@ public class SecurityConfiguration {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.csrf().disable().authorizeHttpRequests()
-                .requestMatchers(HttpMethod.GET, "api/auth/register").permitAll()
-                .requestMatchers(HttpMethod.POST, "api/auth/register").permitAll()
-                .requestMatchers(HttpMethod.GET, "api/auth/authenticate").permitAll()
-                .requestMatchers(HttpMethod.POST, "api/auth/authenticate").permitAll()
-                .anyRequest().authenticated().and().cors()
-                .and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-                .and().authenticationProvider(authenticationProvider).addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
-        return http.build();
+        return http.
+                httpBasic(AbstractHttpConfigurer::disable)
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(authHttpRequest -> authHttpRequest
+                        .requestMatchers("api/auth/register",
+                                "api/auth/authenticate").permitAll()
+                        .requestMatchers("api/materiaprima/**").authenticated()
+                        .requestMatchers("api/user/**").authenticated())
+                .cors(AbstractHttpConfigurer::disable)
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authenticationProvider(authenticationProvider)
+                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)
+                .build();
     }
 }
