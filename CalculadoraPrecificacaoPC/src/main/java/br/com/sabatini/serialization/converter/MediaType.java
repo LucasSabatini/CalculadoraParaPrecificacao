a5f152d2048d48e99a89cbc38caa5337d888f56b
lucassabatini <lucas.frsabatini@gmail.com>
2024-01-30 12:14:50 -0300

    Criação das classes de suporte à Content Negotiation, para a produção e consumo de dados em JSON, XML e YAML via Header Params

---
+++ CalculadoraPrecificacaoPC/src/main/java/br/com/sabatini/serialization/converter/MediaType.java
@@ -0,0 +1,8 @@
+package br.com.sabatini.serialization.converter;
+
+public class MediaType {
+    public static final String APPLICATION_JSON = "application/json";
+    public static final String APPLICATION_XML = "application/xml";
+    public static final String APPLICATION_YML = "application/x-yaml";
+
+}
