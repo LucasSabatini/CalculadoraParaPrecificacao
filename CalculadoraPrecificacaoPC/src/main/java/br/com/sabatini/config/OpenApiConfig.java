5a40e25247b9fe31642a0b36bd9b96392b38f127
lucassabatini <lucas.frsabatini@gmail.com>
2024-02-03 14:41:03 -0300

    Implementação do SecurityScheme para autenticação com token nas requisições HTTP

--- CalculadoraPrecificacaoPC/src/main/java/br/com/sabatini/config/OpenApiConfig.java
+++ CalculadoraPrecificacaoPC/src/main/java/br/com/sabatini/config/OpenApiConfig.java
@@ -29,8 +29,8 @@ public class OpenApiConfig {
                 .info(new Info()
                         .title("Calculadora para a Precificação de Produtos e de Gastos com Matérias-Primas")
                         .version("v1")
-                        .description("O projeto é uma API desenvolvida com o objetivo" +
-                                "de fornecer uma interface para o cálculo realizado" +
+                        .description("O projeto é uma API desenvolvida com o objetivo " +
+                                "de fornecer uma interface para o cálculo realizado " +
                                 "na precificação de produtos para posterior venda e para o " +
                                 "cálculo de gastos com as matérias-primas utilizadas em seus produtos.")
                         .license(new License()
